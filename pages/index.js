ef6d5e4bf593108e176d5bb449860bf00c1a3c2c
evamiliczka <evamiliczka@rarecrew.com>
2025-05-23 12:55:11 +0200

    xxx

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 // pages/index.js
 import React from "react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-const myVariable = process.env.IS_PREVIEW;
+const myVariable = process.env.NEXT_PUBLIC_IS_PREVIEW;
 
-export default () => (< div > Hello World Next JS! {myVariable}  The {process.env.IS_PREVIEW} branch.  <SpeedInsights /> < /div>);
+export default () => (< div > Hello World Next JS! {myVariable}  The {process.env.NEXT_PUBLIC_IS_PREVIEW} branch.  <SpeedInsights /> < /div>);
